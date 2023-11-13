c15b9cc016e8abe3e6c2b0e94db52d64db478c7e
rahidyat <rhmathidyat12@gmail.com>
2023-11-13 14:28:42 +0700

    Updated

--- module5-assigment/js/script.js
+++ module5-assigment/js/script.js
@@ -158,7 +158,7 @@ dc.loadMenuCategories = function () {
 dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
-    menuItemsUrl + categoryShort,
+    menuItemsUrl + categoryShort + ".json",
     buildAndShowMenuItemsHTML);
 };
 
