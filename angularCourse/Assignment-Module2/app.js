5e506778f205773c09c9ade174c353341f35e04d
rahidyat <rhmathidyat12@gmail.com>
2024-01-01 20:22:10 +0700

    finished

---
+++ angularCourse/Assignment-Module2/app.js
@@ -0,0 +1,98 @@
+(function(){
+    'use strict';
+
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+    ToBuyController.$inject=['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService){
+        const buyCtrl = this;
+
+        buyCtrl.items = ShoppingListCheckOffService.getItemsToBuy();        
+
+        buyCtrl.buyItem = function(itemIndex){
+            ShoppingListCheckOffService.addItemBought(itemIndex);                        
+        };
+
+        buyCtrl.everythingBought = function(){
+            return ShoppingListCheckOffService.arrayLength(buyCtrl.items);        
+        };
+    }
+
+
+    AlreadyBoughtController.$inject=['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService){
+        const boughtCtrl = this;
+
+        boughtCtrl.items = ShoppingListCheckOffService.getItemsBought();        
+
+        boughtCtrl.nothingBought = function(){
+            return ShoppingListCheckOffService.arrayLength(boughtCtrl.items);        
+        };
+    }
+
+
+    function ShoppingListCheckOffService(){
+        const service = this;
+
+        let itemsToBuy = [
+            {
+                name: "cookies",
+                quantity: 5
+            },
+            {
+                name: "super cookies",
+                quantity: 15
+            },
+            {
+                name: "chocolate cookies",
+                quantity: 20
+            },
+            {
+                name: "nice cookies",
+                quantity: 10
+            },
+            {
+                name: "many cookies",
+                quantity: 30
+            },
+        ];
+
+        let itemsBought = [];
+
+        service.getItemsToBuy = function(){
+            return itemsToBuy;
+        };
+
+        service.getItemsBought = function(){
+            return itemsBought;
+        };
+
+        service.addItemBought = function(index){
+            let item = {
+                name: itemsToBuy[index].name,
+                quantity: itemsToBuy[index].quantity
+            }
+
+            itemsToBuy.splice(index, 1);
+            itemsBought.push(item);
+
+            console.log(itemsToBuy);
+            console.log(itemsBought);
+        };
+
+        service.arrayLength = function(array){
+            if(array.length == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        };
+    }
+
+
+})();
