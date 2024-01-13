28d5018b46c5f898173e520ff04a6409faee6d0b
rahidyat <rhmathidyat12@gmail.com>
2024-01-13 11:00:07 +0700

    revision 2

--- angularCourse/Assignment-Module3/js/app.js
+++ angularCourse/Assignment-Module3/js/app.js
@@ -5,7 +5,7 @@ angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
 .directive('foundItems', FoundItemsDirective)
-.constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com/menu_items.json');
+.constant('ApiBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json');
 
 function FoundItemsDirective() {
   var ddo = {
